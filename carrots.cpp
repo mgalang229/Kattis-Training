ec4fd0bd09a28de91c938b4ba2b5c35d768614e0
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-09-22 18:11:40 +0800

    Create carrots.cpp

---
+++ carrots.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+#define ll long long
+#define ar array
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	
+	int n, p;
+	cin >> n >> p;
+	for(int i=0; i<n; ++i) {
+		string s;
+		cin >> s;
+	}
+	cout << p << "\n";
+}
