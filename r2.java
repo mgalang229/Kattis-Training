f1876a1267b2c82bc78bab8bc21ec028183080b1
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-12-27 21:10:19 +0800

    Create r2.java

---
+++ r2.java
@@ -0,0 +1,53 @@
+import java.io.*;
+import java.util.*;
+
+public class r2 {
+	public static void main(String[] args) {
+		FastReader in = new FastReader();
+		int r1 = in.nextInt();
+		int s = in.nextInt();
+		System.out.println((2 * s) - r1);
+	}
+	
+	static class FastReader {
+		BufferedReader br;
+		StringTokenizer st;
+		
+		public FastReader() {
+			br = new BufferedReader(new InputStreamReader(System.in));
+		}
+		
+		String next() {
+			while (st == null || !st.hasMoreElements()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch (IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
