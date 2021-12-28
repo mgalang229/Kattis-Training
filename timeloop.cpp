c462ef0d4cf02447c8e4657de10f2a3d844a024e
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2021-12-28 20:28:05 +0800

    Create timeloop.cpp

---
+++ timeloop.cpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(false);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	for (int i = 0; i < n; i++) {
+		cout << (i + 1) << " Abracadabra" << '\n';
+	}
+  return 0;
+}
