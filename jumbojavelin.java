d3b924d27a20f43b52733d30dfac2bdbdc4e0b53
Jason Galang <51401355+mgalang229@users.noreply.github.com>
2022-01-29 22:11:09 +0800

    Create jumbojavelin.java

---
+++ jumbojavelin.java
@@ -0,0 +1,60 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+	public static void main(String[] args) {
+        FastReader in = new FastReader();
+        int n = in.nextInt();
+        int sum = 0;
+        for (int i = 0; i < n; i++) {
+        	sum += in.nextInt();
+        	// after the first rod, start decrementing every rod that will be added
+        	if (i > 0) {
+        		sum--;
+        	}
+        }
+        System.out.println(sum);
+    }
+
+    static class FastReader {
+        BufferedReader br;
+        StringTokenizer st;
+        
+        public FastReader() {
+            br = new BufferedReader(new InputStreamReader(System.in));
+        }
+        
+        String next() {
+            while (st == null || !st.hasMoreElements()) {
+                try {
+                    st = new StringTokenizer(br.readLine());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return st.nextToken();
+        }
+        
+        int nextInt() {
+            return Integer.parseInt(next());
+        }
+        
+        long nextLong() {
+            return Long.parseLong(next());
+        }
+        
+        double nextDouble() {
+            return Double.parseDouble(next());
+        }
+        
+        String nextLine() {
+            String str = "";
+            try {
+                str = br.readLine();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return str;
+        }
+    }
+}
