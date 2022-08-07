84fbe1e7949a36b004ba85af446a558c7c0e68df
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-08-08 02:31:02 +0800

    Create filip.java

---
+++ filip.java
@@ -0,0 +1,124 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.Set;
+import java.util.StringTokenizer;
+import java.util.TreeSet;
+
+public class filip {
+	
+	public static void main(String[] args) {	
+		FastScanner fs = new FastScanner();
+		PrintWriter out = new PrintWriter(System.out);
+		int T = 1;
+		//T = fs.nextInt();
+		for (int tc = 0; tc < T; tc++) {
+			char[] a = fs.next().toCharArray();
+			char[] b = fs.next().toCharArray();
+			reverseArray(a);
+			reverseArray(b);
+			boolean checker = false;
+			for (int i = 0; i < a.length; i++) {
+				if (((int) a[i] - '0') > ((int) b[i] - '0')) {
+					checker = true;
+					break;
+				} else if (((int) a[i] - '0') < ((int) b[i] - '0')) {
+					break;
+				}
+			}
+			if (checker) {
+				out.println(a);
+			} else {
+				out.println(b);
+			}
+		}
+		out.close();
+	}
+	
+	static void reverseArray(char[] arr) {
+		ArrayList<Character> arrList = new ArrayList<>();
+		for (int i = 0; i < arr.length; i++) {
+			arrList.add(arr[i]);
+		}
+		Collections.reverse(arrList);
+		for (int i = 0; i < arr.length; i++) {
+			arr[i] = arrList.get(i);
+		}
+	}
+	
+	static void sort(int[] a) {
+		ArrayList<Integer> arr = new ArrayList<>();
+		for (int x : a) {
+			arr.add(x);
+		}
+		Collections.sort(arr);
+		for (int i = 0; i < a.length; i++) {
+			a[i] = arr.get(i);
+		}
+	}
+	
+	static void swap(int[] a, int x, int y) {
+		int temp = a[x];
+		a[x] = a[y];
+		a[y] = temp;
+	}
+	
+	static class FastScanner {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer("");
+		
+		String next() {
+			while (!st.hasMoreTokens()) {
+				try {
+					st = new StringTokenizer(br.readLine());
+				} catch (IOException e) {
+					e.printStackTrace();
+				}
+			}
+			return st.nextToken();
+		}
+		
+		int nextInt() {
+			return Integer.parseInt(next());
+		}
+		
+		int[] readArray(int n) {
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextInt();
+			}
+			return a;
+		}
+		
+		long[] readLongArray(int n) {
+			long[] a = new long[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = nextLong();
+			}
+			return a;
+		}
+		
+		long nextLong() {
+			return Long.parseLong(next());
+		}
+		
+		double nextDouble() {
+			return Double.parseDouble(next());
+		}
+		
+		String nextLine() {
+			String str = "";
+			try {
+				str = br.readLine();
+			} catch(IOException e) {
+				e.printStackTrace();
+			}
+			return str;
+		}
+	}
+}
