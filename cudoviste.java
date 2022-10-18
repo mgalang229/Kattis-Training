c644d6ec4b4cdd7c558c5ba8e912ef1ffac86486
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-10-18 23:40:29 +0800

    Update cudoviste.java

--- cudoviste.java
+++ cudoviste.java
@@ -1,2 +1,45 @@
+import java.util.Scanner;
 
-javaasdasdasasddsadasdas
+// Problem link: https://open.kattis.com/problems/cudoviste
+
+public class cudoviste {
+    public static void main(String args[]) {
+        Scanner fs = new Scanner(System.in);
+        int n = fs.nextInt(), m = fs.nextInt();
+        char[][] s = new char[n][];
+        for (int i = 0; i < n; i++) {
+            s[i] = fs.next().toCharArray();
+        }
+        for (int squash = 0; squash < 5; squash++) {
+            int count = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    if (s[i][j] == '#') {
+                        continue;
+                    }
+                    if (i + 1 < n && j + 1 < m) {
+                        int curSquash = 0;
+                        outer: for (int k = i; k <= i + 1; k++) {
+                            for (int l = j; l <= j + 1; l++) {
+                                if (s[k][l] == 'X') {
+                                    curSquash++;
+                                }
+                                if (s[k][l] == '#') {
+                                    curSquash = -1;
+                                    break outer;
+                                }
+                            }
+                        }
+                        // System.out.println(curSquash + " , " + squash);
+                        if (curSquash == squash) {
+                            count++;
+                        }
+                    }
+                }
+                // System.out.println();
+            }
+            System.out.println(count);
+        }
+        fs.close();
+    }
+}
