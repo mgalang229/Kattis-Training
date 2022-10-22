3dfbd18ca7cefc0dceb665449cd7e400af4ab002
jasonpogi <51401355+mgalang229@users.noreply.github.com>
2022-10-22 18:16:32 +0800

    Update parking2.java

--- parking2.java
+++ parking2.java
@@ -1,1 +1,25 @@
-asddasds
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class parking2 {
+	
+	public static void main(String[] args) {
+		Scanner fs = new Scanner(System.in);
+		int T = fs.nextInt();
+		for (int tc = 1; tc <= T; tc++) {
+			int n = fs.nextInt();
+			int[] a = new int[n];
+			for (int i = 0; i < n; i++) {
+				a[i] = fs.nextInt();
+			}
+			Arrays.sort(a);
+			int ans = 0;
+			for (int i = 1; i < n; i++) {
+				ans += Math.abs(a[i] - a[i-1]);
+			}
+                        //return back to the parking slot (beginnning)
+			System.out.println(ans * 2);
+		}
+		fs.close();
+	}
+}
